c80d2429b9af70b5ae3f8a87a801d5133e3a0f2f
“sandy1618“ <sandeepnayak1.618@gmail.com>
2023-05-18 11:30:53 +0900

    [QR code] qr code & beautification done

--- connectx/signup/models.py
+++ connectx/signup/models.py
@@ -7,6 +7,15 @@ class UserProfile(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=128)
+   
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class ProfileLink(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='profile_links')
+    link_category = models.CharField(max_length=100,default='')
+    link = models.URLField(default='')
+
+    def __str__(self):
+        return f"{self.link_category}: {self.link}"
--- connectx/signup/views.py
+++ connectx/signup/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password, check_password
-from .models import UserProfile
+from .models import UserProfile, ProfileLink
 
 def signup(request):
     if request.method == 'POST':
@@ -75,7 +75,27 @@ def profile(request, pk):
 
     return render(request, 'signup/profile.html', {'user': user})
 
+def admin_page(request, pk):
+    user = UserProfile.objects.get(pk=pk)
 
+    if request.method == 'POST':
+        link_category = request.POST.get('link_category')
+        link = request.POST.get('link')
+
+        # Create a new UserLink instance and save it to the database
+        user_link = ProfileLink.objects.create(user_profile=user, link_category=link_category, link=link)
+
+        # Redirect back to the admin page or any other desired page
+        return redirect('signup:admin', pk=pk)
+
+    return render(request, 'signup/admin.html', {'user': user})
+
+
+def portfolio_page(request, pk):
+    user = UserProfile.objects.get(pk=pk)
+    links = ProfileLink.objects.filter(user_profile=user)
+    context = {'user': user, 'links': links}
+    return render(request, 'signup/portfolio.html', context)
 # def profile(request, pk):
 #     user = get_object_or_404(UserProfile, pk=pk)
 
